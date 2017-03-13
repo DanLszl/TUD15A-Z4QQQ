a93b0a13a7589d1d461aeef13e9c54fb118992e2
Peter Eder <petereder89@gmail.com>
2017-03-13 16:22:10 +0100

    basic project setup

---
+++ pong/src/main/java/athens/org/App.java
@@ -0,0 +1,13 @@
+package athens.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
