5d9aaab977ca69d2bfd2c33cbaf322421c46d561
Peter Eder <petereder89@gmail.com>
2017-03-13 16:48:52 +0100

    basic slick-2d project setup, not working yet

--- pong/src/main/java/athens/org/App.java
+++ pong/src/main/java/athens/org/App.java
@@ -1,13 +1,45 @@
 package athens.org;
 
+import org.newdawn.slick.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends BasicGame
 {
-    public static void main( String[] args )
+    public App(String gamename)
     {
-        System.out.println( "Hello World!" );
+        super(gamename);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {}
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {}
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException
+    {
+        g.drawString("Howdy!", 10, 10);
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new App("Simple Slick Game"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
