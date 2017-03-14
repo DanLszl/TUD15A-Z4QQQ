07011d1e093bad1da1430f040b02784082085d8f
Peter Eder <petereder89@gmail.com>
2017-03-14 12:35:30 +0100

    added score board

--- pong/src/main/java/athens/org/states/PlayingState.java
+++ pong/src/main/java/athens/org/states/PlayingState.java
@@ -1,15 +1,17 @@
 package athens.org.states;
 
 import athens.org.Ball;
+import athens.org.Constants;
 import athens.org.GameBorder;
 import athens.org.Paddle;
 import org.lwjgl.input.Keyboard;
-import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.*;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+
+import java.awt.*;
+import java.awt.Font;
 
 /**
  * Created by peter on 14.03.17.
@@ -17,12 +19,16 @@ import org.newdawn.slick.state.StateBasedGame;
 public class PlayingState extends BasicGameState {
 
     public static final int ID=1;
+    private static final int WINS_NEEDED=5;
 
     private Paddle paddleLeft;
     private Paddle paddleRight;
 
     private GameBorder border;
     private Input input;
+
+    private Font myFont2;
+    private TrueTypeFont font2;
 
     Ball ball;
 
@@ -44,6 +50,9 @@ public class PlayingState extends BasicGameState {
         border = new GameBorder(640f, 480f);
 
         input = gameContainer.getInput();
+
+        myFont2= new Font("Verdana", Font.BOLD, 30);
+        font2=new TrueTypeFont(myFont2, false);
     }
 
     @Override
@@ -53,6 +62,12 @@ public class PlayingState extends BasicGameState {
         border = new GameBorder(640f, 480f);
 
         input = gameContainer.getInput();
+
+        //render scoreboard centered
+        String scoreBoard=paddleLeft.getScore()+" : " +paddleRight.getScore();
+        int scoreBoardWidth=font2.getWidth(scoreBoard);
+        graphics.setFont(font2);
+        graphics.drawString(scoreBoard, (Constants.SCREEN_WIDTH-scoreBoardWidth)/2,20);
 
     }
 
@@ -74,6 +89,20 @@ public class PlayingState extends BasicGameState {
         if (input.isKeyDown(Keyboard.KEY_W) && !border.intersectUpper(paddleLeft)) {
             movePaddleUp(distance, paddleLeft);
         }
+
+
+
+        
+
+        //check if a player already one, TODO: pass player that one to gameoverscreen
+        if(paddleLeft.getScore()>=WINS_NEEDED){
+            stateBasedGame.enterState(GameOverState.ID);
+        }
+
+        if(paddleRight.getScore()>=WINS_NEEDED){
+            stateBasedGame.enterState(GameOverState.ID);
+        }
+
     }
 
     private void movePaddleDown(float distance, Paddle paddle) {
