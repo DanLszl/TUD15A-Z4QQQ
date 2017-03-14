b63a16872b92090decacde64a4cf6036f38a47dd
bereczdaniel <berecz.17@gmail.com>
2017-03-14 11:48:48 +0100

    b

--- pong/src/main/java/athens/org/GameBorder.java
+++ pong/src/main/java/athens/org/GameBorder.java
@@ -11,17 +11,33 @@ public class GameBorder{
 
     private Line upperBound;
     private Line lowerBound;
+    private Line rightBound;
+    private Line leftBound;
 
     public GameBorder(float width, float height){
         upperBound = new Line(new Vector2f(0,0), new Vector2f(640, 0));
         lowerBound = new Line(new Vector2f(0,480), new Vector2f(640, 480));
+        rightBound = new Line(new Vector2f(640, 0), new Vector2f(640, 480));
+        leftBound = new Line(new Vector2f(0,0), new Vector2f(0, 480));
     }
 
     public boolean intersectUpper(Shape shape){
+
         return upperBound.intersects(shape);
     }
 
     public boolean intersectLower(Shape shape){
+
         return lowerBound.intersects(shape);
+    }
+
+    public boolean intersectRight(Shape shape) {
+
+        return rightBound.intersects(shape);
+    }
+
+    public boolean intersectLeft(Shape shape){
+
+        return leftBound.intersects(shape);
     }
 }
