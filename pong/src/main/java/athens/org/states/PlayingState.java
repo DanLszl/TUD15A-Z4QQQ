fb56f21f0b56d9e2c836780fceb73abada4f378a
Peter Eder <petereder89@gmail.com>
2017-03-14 11:05:16 +0100

    added logic to gamestate

--- pong/src/main/java/athens/org/states/PlayingState.java
+++ pong/src/main/java/athens/org/states/PlayingState.java
@@ -1,7 +1,11 @@
 package athens.org.states;
 
+import athens.org.Ball;
+import athens.org.Paddle;
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -12,6 +16,11 @@ import org.newdawn.slick.state.StateBasedGame;
 public class PlayingState extends BasicGameState {
 
     public static final int ID=1;
+
+    private Paddle paddleLeft;
+    private Paddle paddleRight;
+
+    Ball ball;
 
 
     public PlayingState(){
@@ -25,16 +34,41 @@ public class PlayingState extends BasicGameState {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-
+        paddleLeft = new Paddle(50f);
+        paddleRight = new Paddle(590f);
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-    //    graphics.drawString("test",100,100);
+        graphics.fill(paddleLeft);
+        graphics.fill(paddleRight);
     }
 
     @Override
-    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
+        int speed = 100;
+        Input input=gameContainer.getInput();
 
+        float distance = speed * delta / 1000f;
+        if (input.isKeyDown(Keyboard.KEY_UP)) {
+            movePaddleUp(distance, paddleRight);
+        }
+        if (input.isKeyDown(Keyboard.KEY_DOWN)) {
+            movePaddleDown(distance, paddleRight);
+        }
+        if(input.isKeyDown(Keyboard.KEY_S)){
+            movePaddleDown(distance, paddleLeft);
+        }
+        if(input.isKeyDown(Keyboard.KEY_W)){
+            movePaddleUp(distance, paddleLeft);
+        }
+    }
+
+    private void movePaddleDown(float distance, Paddle paddle) {
+        paddle.setCenterY(paddle.getCenterY() + distance);
+    }
+
+    private void movePaddleUp(float distance, Paddle paddle) {
+        paddle.setCenterY(paddle.getCenterY() - distance);
     }
 }
