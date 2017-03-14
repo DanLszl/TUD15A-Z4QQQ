7de6e0a3434f1b9efda89dfd9fef20ab64460ed5
Peter Eder <petereder89@gmail.com>
2017-03-14 09:49:29 +0100

    implemented welcome screen

--- pong/src/main/java/athens/org/App.java
+++ pong/src/main/java/athens/org/App.java
@@ -1,7 +1,10 @@
 package athens.org;
 
+import athens.org.states.WelcomeState;
 import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
 
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,32 +12,28 @@ import java.util.logging.Logger;
  * Hello world!
  *
  */
-public class App extends BasicGame
+public class App extends StateBasedGame
 {
     public App(String gamename)
     {
         super(gamename);
     }
 
-    @Override
-    public void init(GameContainer gc) throws SlickException {}
 
     @Override
-    public void update(GameContainer gc, int i) throws SlickException {}
-
-    @Override
-    public void render(GameContainer gc, Graphics g) throws SlickException
-    {
-        g.drawString("Howdy!", 10, 10);
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        this.addState(new WelcomeState());
     }
+
 
     public static void main(String[] args)
     {
+
         try
         {
             AppGameContainer appgc;
             appgc = new AppGameContainer(new App("Simple Slick Game"));
-            appgc.setDisplayMode(640, 480, false);
+            appgc.setDisplayMode(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, false);
             appgc.start();
         }
         catch (SlickException ex)
